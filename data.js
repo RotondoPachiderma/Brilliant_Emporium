3090d8e905777a00e7b331073f6c70c387c376e6
RotondoPachiderma <rotondopachiderma@gmail.com>
2025-11-02 03:52:39 +0100

    opted for js instead of jsons, no database anymore

---
+++ data.js
@@ -0,0 +1,65 @@
+const products = [
+  {
+    name: "Verità",
+    costo: 10,
+    descrizione: "risposta sincera ed esaustiva ad una qualsiasi domanda",
+    rarita: "comune",
+    ripetibile: true,
+    immagine: "🤥",
+    categoria: ["verbale", "ripetibile"]
+  },
+  {
+    name: "Sticker incredibile",
+    costo: 100,
+    descrizione: "sticker incredibile di un mio costume di Halloween",
+    rarita: "epico",
+    ripetibile: false,
+    immagine: "👗",
+    categoria: ["media", "collezionabile"]
+  },
+  {
+    name: "Non per vantarmi",
+    costo: 25,
+    descrizione: "foto di me con i capelli rosa",
+    rarita: "raro",
+    ripetibile: false,
+    immagine: "🌈",
+    categoria: ["media", "collezionabile"]
+  },
+  {
+    name: "Metallaro",
+    costo: 40,
+    descrizione: "gif di me con i capelli lunghi",
+    rarita: "raro",
+    ripetibile: false,
+    immagine: "👩",
+    categoria: ["media", "collezionabile"]
+  },
+  {
+    name: "Complimento sincero",
+    costo: 15,
+    descrizione: "ti faccio un complimento sincero e non scherzoso",
+    rarita: "comune",
+    ripetibile: true,
+    immagine: "❤️",
+    categoria: ["verbale", "ripetibile"]
+  },
+  {
+    name: "Set Lucine",
+    costo: 190,
+    descrizione: "ti regalo un set di lucine particolari",
+    rarita: "epico",
+    ripetibile: false,
+    immagine: "✨",
+    categoria: ["regalo"]
+  },
+  {
+    name: "Oggetto personalizzato",
+    costo: 100,
+    descrizione: "oggetto personalizzato fatto a mano da me",
+    rarita: "epico",
+    ripetibile: false,
+    immagine: "🛠️",
+    categoria: ["regalo"]
+  }
+];
