9f87f4e5ee8ed4af157993bcdcab779947d145fd
RotondoPachiderma <rotondopachiderma@gmail.com>
2025-11-02 05:56:17 +0100

    filters now functioning

--- script.js
+++ script.js
@@ -30,10 +30,32 @@ function filtersCreator(products){
     })
     filters.forEach(filter =>{
         let button = `
-        <button class="filter-button">${filter}</button>`
+        <button class="filter-button" onclick="filtering('${filter}', products)">${filter}</button>`
         html += button
     })
     return html
+}
+
+function filtering(filter, products){
+    let html = ""
+    products.forEach(product =>{
+        if (product.categoria.includes(filter)){
+            let productTab = `
+            <div class="items">
+                <p class="item-photo"> ${product.immagine}</p>
+                <div class="item-text">
+                    <div class="item-name-rarity">
+                        <p class="item-name">${product.name}</p>
+                    </div>
+                    <p class="item-desc"> ${product.descrizione}</p>
+                </div>
+                <button class="item-price">${product.costo}pt</button>
+                <button class="item-buy">Compra</button>
+            </div>`
+        html += productTab
+        }
+    })
+    htmlInjector(html, ".js-catalogo-wrapper")
 }
 
 function htmlInjector(element, locationClass){
