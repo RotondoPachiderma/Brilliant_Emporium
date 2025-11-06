4123bd2e56ffce09e8f161b5747315a1264fd788
RotondoPachiderma <rotondopachiderma@gmail.com>
2025-11-06 02:33:32 +0100

    Buy functionalities finished and refactored code

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
-function shopCreator(products){
-    let html = ""
-    products.forEach(product =>{
-        let productTab = `
+//Ritorna l'oggetto di un prodotto dato il suo id in numero o stringa
+function objectByUuid(id){
+    id = Number(id)
+    return products.find(product => product.id === id)
+}
+
+
+function htmlInjector(element, locationClass){
+    const location = document.querySelector(locationClass)
+    location.innerHTML = element
+}
+
+//Dato un oggetto prodotto ritorna l'HTML completo della tab di quel prodotto
+function productHtmlGenerator(product){
+    let productTab = `
         <div class="items">
             <p class="item-photo"> ${product.immagine}</p>
             <div class="item-text">
@@ -10,14 +21,23 @@ function shopCreator(products){
                 </div>
                 <p class="item-desc"> ${product.descrizione}</p>
             </div>
-            <button class="item-price">${product.costo}pt</button>
-            <button class="item-buy js-${product.name}-buy" onclick="productButtonFunction('${product.name}')">Compra</button>
+            <button class="item-price js-${product.id}-price">${product.costo}pt</button>
+            <button class="item-buy js-${product.id}-buy" onclick="buyButton(${product.id})">Compra</button>
         </div>`
+    return productTab
+}
+
+//Dato un array di prodotti ritorna l'html dello shop
+function shopCreator(products){
+    let html = ""
+    products.forEach(product =>{
+        let productTab = productHtmlGenerator(product)
         html += productTab
     })
     return html
 }
 
+//Dato un array di prodotti ritorna l'html dei filtri
 function filtersCreator(products){
     let filters = []
     let html = ""
@@ -36,28 +56,19 @@ function filtersCreator(products){
     return html
 }
 
+//Data una stringa filtro prende l'array di prodotti e genera le tab solo dei prodotti con quel filtro, poi lo inietta nella pagina
 function filtering(filter, products){
     let html = ""
     products.forEach(product =>{
         if (product.categoria.includes(filter)){
-            let productTab = `
-            <div class="items">
-                <p class="item-photo"> ${product.immagine}</p>
-                <div class="item-text">
-                    <div class="item-name-rarity">
-                        <p class="item-name">${product.name}</p>
-                    </div>
-                    <p class="item-desc"> ${product.descrizione}</p>
-                </div>
-                <button class="item-price">${product.costo}pt</button>
-                <button class="item-buy js-${product.name}-buy">Compra</button>
-            </div>`
+            let productTab = productHtmlGenerator(product)
         html += productTab
         }
     })
     htmlInjector(html, ".js-catalogo-wrapper")
 }
 
+//Data una stringa filtro prende il bottone con quel filtro nella classe e lo attiva ricreando la pagina dello shop. Disattiva gli altri bottoni
 function filterButtonFunction(filter){
     let bottone = document.querySelector(`.js-${filter}-button`)
     if (!bottone.classList.contains("filter-button-active")){
@@ -72,12 +83,53 @@ function filterButtonFunction(filter){
     }
 }
 
-function productButtonFunction(product){
-    let bottone = document.querySelector(`.js-${product}-button`)
+//Dato l'id di un prodotto inietta il nome del prodotto nella scheda di checkout e imposta il suo costo
+function buyButton(productId){
+    let popup = document.querySelector(".buy-overlay")
+    let confirmationProduct = document.querySelector(".confirmation-product")
+    let product = objectByUuid(productId)
+    confirmationProduct.innerHTML = `${product.name}`
+    popup.classList.remove("hidden")
+    obscuredOverlay.classList.remove("hidden")
+    currentCost = product.costo
+}
+
+function confirmationButtonFunction(youSure, cost){
+    let popup = document.querySelector(".buy-overlay")
+    let thankspopup = document.querySelector(".thanks")
+    let brokepopup = document.querySelector(".broke")
+    let balance = document.querySelector(".balance")
+    popup.classList.add("hidden")
+    if (!youSure){
+        obscuredOverlay.classList.add("hidden")
+        return
+    }
+    if (youBroke()){
+        brokepopup.classList.remove("hidden")
+        setTimeout(()=>{
+            brokepopup.classList.add("hidden")
+            obscuredOverlay.classList.add("hidden")
+        },3000)
+        return
+    }
+    thankspopup.classList.remove("hidden")
+    setTimeout(()=>{
+        thankspopup.classList.add("hidden")
+        obscuredOverlay.classList.add("hidden")
+    },1000)
+    balance.innerHTML = Number(balance.innerHTML) - cost
+    localStorage.setItem("balance", `${balance.innerHTML}`)
+}
+
+function youBroke(){
+    if (currentCost > document.querySelector(".balance").innerHTML){return true}
 }
 
 
-function htmlInjector(element, locationClass){
-    const location = document.querySelector(locationClass)
-    location.innerHTML = element
+function balanceSetter(){
+    let balanceDisplay = document.querySelector(".balance")
+    if (localStorage.getItem("balance")){
+        balanceDisplay.innerHTML=Number(localStorage.getItem("balance"))
+    }
+    else {return}
 }
